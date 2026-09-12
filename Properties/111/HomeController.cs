ccb197559d09b4f6450e29eb859fe802e8b7ac27
Hoang Nguyen <rrgyfdeds@gmail.com>
2026-09-12 22:16:36 +0700

    test commit

---
+++ Properties/111/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JCAP.Properties._111
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
