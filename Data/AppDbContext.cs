a900b89986785ac4bd434e138e961afeaf431daf
AkaiZed <wtf0888506598@gmail.com>
2026-09-16 16:34:03 +0700

    feat: add credit balance and credit system migration

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,12 +10,47 @@ namespace JCAP.Data
         {
         }
 
+        public DbSet<CreditPackage> CreditPackages { get; set; }
+        public DbSet<CreditTransaction> CreditTransactions { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            // Các cấu hình Fluent API sau này cho các Entity mới sẽ được đặt ở đây
+            // CreditPackage
+            modelBuilder.Entity<CreditPackage>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Price)
+                    .HasPrecision(18, 2);
+            });
+
+            // CreditTransaction
+            modelBuilder.Entity<CreditTransaction>(entity =>
+            {
+                entity.Property(e => e.Type)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.Status)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.PayOsOrderCode)
+                    .HasMaxLength(100);
+
+                // ApplicationUser 1 - N CreditTransaction
+                entity.HasOne(e => e.User)
+                    .WithMany()
+                    .HasForeignKey(e => e.UserId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
         }
     }
 }
-
